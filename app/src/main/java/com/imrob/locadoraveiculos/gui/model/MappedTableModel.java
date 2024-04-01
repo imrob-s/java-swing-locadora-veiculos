8e74693ee115ba7788feda94d037d04d2d56c000
imrob-s <orobsilva@gmail.com>
2024-04-01 14:48:41 -0300

    add: javadoc in MappedTableModel class

--- app/src/main/java/com/imrob/locadoraveiculos/gui/model/MappedTableModel.java
+++ app/src/main/java/com/imrob/locadoraveiculos/gui/model/MappedTableModel.java
@@ -6,18 +6,37 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Um modelo de tabela genérico para mapear entidades para uma tabela Swing.
+ *
+ * Obs: Os metodos gettters da entidade devem sequir a ordem das colunas da tabela.
+ *
+ * @param <ENTITY> o tipo da entidade a ser mapeada
+ */
 public class MappedTableModel<ENTITY> extends AbstractTableModel {
     private List<ENTITY> lista;
     private String[] columnNames;
     private List<String> metodos;
 
+    /**
+     * Constrói um modelo de tabela mapeado com as entidades fornecidas.
+     * As colunas da tabela serão nomeadas de acordo com os nomes dos atributos da entidade.
+     *
+     * @param entities a lista de entidades a serem mapeadas
+     */
     public MappedTableModel(List<ENTITY> entities){
         lista = entities;
         columnNames = getColumnNames();
         metodos = new ArrayList<>();
         setMetodos();
     }
-    
+
+    /**
+     * Constrói um modelo de tabela mapeado com as entidades fornecidas e os nomes das colunas especificados.
+     *
+     * @param entities a lista de entidades a serem mapeadas
+     * @param columnNames os nomes das colunas da tabela
+     */
     public MappedTableModel(List<ENTITY> entities, String[] columnNames){
         lista = entities;
         this.columnNames = columnNames;
@@ -25,6 +44,11 @@ public class MappedTableModel<ENTITY> extends AbstractTableModel {
         setMetodos();
     }
 
+    /**
+     * Obtém os nomes das colunas da tabela com base nos atributos da entidade.
+     *
+     * @return um array de strings contendo os nomes das colunas da tabela
+     */
     public String[] getColumnNames() {
         Field[] campos = lista.get(0).getClass().getDeclaredFields();
         String[] colunas = new String[campos.length];
@@ -35,11 +59,17 @@ public class MappedTableModel<ENTITY> extends AbstractTableModel {
         return colunas;
     }
 
+    /*
+     * Obtém o número de linhas da tabela.
+     */
     @Override
     public int getRowCount() {
         return lista.size();
     }
 
+    /*
+     * Obtém o número de colunas da tabela.
+     */
     @Override
     public int getColumnCount() {
         return columnNames.length;
@@ -49,7 +79,12 @@ public class MappedTableModel<ENTITY> extends AbstractTableModel {
     public String getColumnName(int column) {
         return columnNames[column];
     }
-
+    /**
+     * Obtém o valor contido em uma determinada posição da tabela.
+     *
+     * @param rowIndex a linha da tabela
+     * @param columnIndex a coluna da tabela
+     */
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         try {
@@ -60,7 +95,10 @@ public class MappedTableModel<ENTITY> extends AbstractTableModel {
             return null;
         }
     }
-    
+
+    /**
+     * Define os nomes dos metodos da entidade.
+     */
     private void setMetodos(){
         Method[] arrayMethod = lista.get(0).getClass().getDeclaredMethods();
 
@@ -71,7 +109,12 @@ public class MappedTableModel<ENTITY> extends AbstractTableModel {
             }
         }
     }
-    
+
+    /**
+     * Define os nomes das colunas da tabela.
+     *
+     * @param columnNames os nomes das colunas da tabela
+     */
     public void setColumnNames(String[] columnNames){
         this.columnNames = columnNames;
     }
